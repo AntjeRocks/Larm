4b87a196e910098c747e9627f0a65a4bbde57a0d
Antje <fupsychen@web.de>
2017-11-19 16:10:07 +0100

    add project and smtp setup

---
+++ larm.py
@@ -0,0 +1,56 @@
+import time
+import os
+from smtplib import SMTP
+
+
+def larm(description, value, soft_limit, hard_limit):
+    if value > hard_limit:
+        error(description, str(value))
+    elif value > soft_limit:
+        warn(description, str(value))
+
+
+def error(description, value):
+    log = log_line("Error  ", description, value)
+    print(log)
+    add_log_entry(log + "\n")
+    send_mail(log)
+
+
+def warn(description, value):
+    log = log_line("Warning", description, value)
+    print(log)
+    add_log_entry(log + "\n")
+
+
+def log_line(type, description, value):
+    return "[" + get_time() + "] " + type + " |" + network_name() + "| " + description + ": " + str(value)
+
+
+def add_log_entry(log):
+    file = open('log.txt', 'a')
+    file.write(log)
+    file.close()
+
+
+def send_mail(log):
+    smtp = SMTP()
+    smtp.connect('localhost', 1025)
+
+    from_addr = "Cooler Admin <cooler@dmin.org>"
+    to_addr = "chef <chef@firm.org>"
+    subj = "Error"
+    msg = "From: " + from_addr + "\nTo: " + to_addr +"\nSubject: " + subj + "\n\n" + log + "\n"
+
+    smtp.sendmail(from_addr, to_addr, msg)
+    smtp.quit()
+
+
+def get_time():
+    return time.strftime("%Y-%m-%d %H:%M")
+
+
+def network_name():
+    info = os.uname()
+    hostname = info[1]
+    return hostname
---
+++ monitoring.py
@@ -0,0 +1,40 @@
+import larm
+import shutil
+import psutil # must be installed
+import sys
+
+
+def parse_options():
+    if len(sys.argv) == 1:
+        disk_usage()
+        memory_usage()
+        exit(0)
+    if len(sys.argv) == 3 and sys.argv[1] == "-o":
+        if (sys.argv[2] == "disk"):
+            disk_usage()
+        elif (sys.argv[2] == "mem"):
+            memory_usage()
+        else:
+            print("Invalid option: " + sys.argv[2])
+            exit(1)
+        exit(0)
+    print("Usage: " + sys.argv[0] + " (-o <type>)")
+    print("Where type is: disk, memory")
+    exit(1)
+
+
+def disk_usage():
+    print("Checking disk usage...")
+    # TODO - Check for os (os.name?) and use C: on windows
+    usage = shutil.disk_usage("/")
+    percentage = usage.used / usage.total * 100
+    larm.larm("Dateisystem verwendeter Speicherplatz in %", round(percentage), 80, 90)
+
+
+def memory_usage():
+    print("Checking memory usage...")
+    mem = psutil.virtual_memory()
+    larm.larm("Speicherverbrauch in %", round(mem.percent), 50, 60)
+
+
+parse_options()
