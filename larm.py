58303df98f0092774b3f95a24a560e64bac63095
Antje <fupsychen@web.de>
2017-11-27 21:14:28 +0100

    fix larm

--- larm.py
+++ larm.py
@@ -4,12 +4,12 @@ from smtplib import SMTP
 
 
 def larm(description, value, soft_limit, hard_limit):
-    if value < hard_limit:
-        print("Everything is fine   :) \n")
-    elif value > hard_limit:
+    if value > hard_limit:
         error(description, str(value))
     elif value > soft_limit:
         warn(description, str(value))
+    elif value < hard_limit:
+        print("Everything is fine   :) \n")
 
 def error(description, value):
     log = log_line("Error  ", description, value)
