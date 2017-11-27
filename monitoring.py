4abfa8db3f51f7d47dfcefaaa90c0cd1ad8f0f29
Antje <fupsychen@web.de>
2017-11-27 21:01:23 +0100

    add text for threshold

--- monitoring.py
+++ monitoring.py
@@ -4,6 +4,11 @@ import psutil # must be installed
 import sys
 import os
 
+soft_disk_usage = 50
+hard_disk_usage = 90
+soft_memory_usage = 50
+hard_memory_usage = 90
+
 def parse_options():
     if len(sys.argv) == 1:
         disk_usage()
@@ -23,17 +28,29 @@ def parse_options():
     exit(1)
 
 def disk_usage():
+    print_disk_info()
     print("Checking disk usage...")
     if os.name == 'nt':
         usage = shutil.disk_usage("C:")
     else:
         usage = shutil.disk_usage("/")
     percentage = usage.used / usage.total * 100
-    larm.larm("Dateisystem verwendeter Speicherplatz in %", round(percentage), 80, 90)
+    larm.larm("Dateisystem verwendeter Speicherplatz in %", round(percentage), soft_disk_usage, hard_disk_usage)
 
 def memory_usage():
+    print_memory_info()
     print("Checking memory usage...")
     mem = psutil.virtual_memory()
-    larm.larm("Speicherverbrauch in %", round(mem.percent), 50, 60)
+    larm.larm("Speicherverbrauch in %", round(mem.percent), soft_memory_usage, hard_memory_usage)
+
+def print_disk_info():
+    print("Disk Usage Threshold:")
+    print("Softlimit Disk Usage: ", soft_disk_usage)
+    print("Hardlimit Disk Usage: ", hard_disk_usage)
+
+def print_memory_info():
+    print("Memory Usage Threshold:")
+    print("Softlimit Memory Usage: ", soft_memory_usage)
+    print("Hardlimit Memory Usage: ", hard_memory_usage)
 
 parse_options()
